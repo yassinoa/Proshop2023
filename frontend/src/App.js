c833ba7907298f13a75202e4fdaf926c493b6ac8
yassinoa <yessinedev6@gmail.com>
2023-07-02 02:55:42 +0100

    link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+
+import { Outlet } from 'react-router-dom'
 
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <Header/>
       <main className='py-3'>
         <Container>
-          <HomeScreen/>
+          <Outlet/>
         </Container>
       </main> 
       <Footer/>
